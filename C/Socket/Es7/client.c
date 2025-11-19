c24bf7705179b016c632136ec9d1a3bb0af99e8d
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2025-11-19 11:58:36 +0100

    Add files via upload

--- C/Socket/Es7/client.c
+++ C/Socket/Es7/client.c
@@ -15,14 +15,36 @@
 
 int main()
 {
-
     struct sockaddr_in servizio;
     char stringa[20];
-    int socketfd, soa, fromlen = sizeof(servizio);
+    int socketfd, fromlen = sizeof(servizio);
 
+    // Assegnazione valori alla struttura (dominio, indirizzo, porta)
     servizio.sin_family = AF_INET;
     servizio.sin_addr.s_addr = htonl(INADDR_ANY);
     servizio.sin_port = htons(SERVERPORT);
 
+    // Creazione della socket
+    if ((socketfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        printf("Chiamata fallita alla system call socket");
+        exit(0);
+    }
+
+    if((connect(socketfd, (struct sockaddr*)&servizio, sizeof(servizio))) == -1){
+        printf("Chiamata fallita alla system call connect");
+        exit(0);
+    }
+
+    printf("Inserisci una stringa\n");
+    scanf("%s",stringa);
+
+    // Invio la stringa al server
+    write(socketfd, stringa, sizeof(stringa));
+    read(socketfd, stringa, sizeof(stringa));
+
+    printf("Stringa ricevuta %s",stringa);
+
+    close(socketfd);
     return 0;
 }
