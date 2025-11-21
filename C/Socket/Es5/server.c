11bb7ccde17b335b6ebef359058eec3501e4cb3f
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2025-11-21 21:18:47 +0100

    Update server.c

--- C/Socket/Es5/server.c
+++ C/Socket/Es5/server.c
@@ -1,86 +1,73 @@
-/*Esercizio 5
-Scrivere il codice in C, di un applicazione Socket CLIENT-SERVER in cui il server riceve in input 2
-stringhe e, dopo aver effettuato gli eventuali ed opportuni controlli (se necessari), rispedisce al Client il
-messaggio di quale delle due stringhe è + lunga o più corta o se sono uguali.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <netinet/in.h>
 #include <string.h>
-#include <errno.h>
-#include <ctype.h>
 #include <unistd.h>
 
 #define SERVERPORT 1450
 #define DIM 20
 
-int controlloStringhe(char s[], char s2[]){
-    if(strlen(s) > strlen(s2)){
+int controlloStringhe(char s[], char s2[]) {
+    if (strlen(s) > strlen(s2))
         return 1;
-    }else if(strlen(s) < strlen(s2)){
+    else if (strlen(s) < strlen(s2))
         return 0;
-    }else {
+    else
         return -1;
-    }
 }
 
-int main()
-{
-    struct sockaddr_in = servizio;
-    int socketfd, soa, fromlen = sizeof(servizio);
-    char s [DIM], s2 [DIM];
+int main() {
+    struct sockaddr_in servizio;
+    int socketfd, soa;
+    char s[DIM], s2[DIM], risposta[DIM];
+    socklen_t fromlen = sizeof(servizio);
+
     servizio.sin_family = AF_INET;
     servizio.sin_addr.s_addr = htonl(INADDR_ANY);
     servizio.sin_port = htons(SERVERPORT);
-    
-    if((socketfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-        printf("Chiamata fallita alla system call socket");
-        exit(0);
+
+    if ((socketfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
     }
-    
-    if((bind(socketfd, (struct sockaddr*)&servizio, &fromlen)) == -1){
-        printf("Chiamata fallita alla system call bind");
-        exit(0);
+
+    if (bind(socketfd, (struct sockaddr*)&servizio, sizeof(servizio)) == -1) {
+        perror("bind");
+        exit(1);
     }
-    
-    if((listen(socketfd, 10)) == -1){
-        printf("Chiamata fallita alla system call listen");
-        exit(0);
+
+    if (listen(socketfd, 10) == -1) {
+        perror("listen");
+        exit(1);
     }
-    
-    for(;;){
-        printf("Server in ascolto");
-        
-        if((soa = accept(socketfd, (struct sockaddr*)&servizio, &fromlen)) == -1){
-            printf("Chiamata fallita alla system call accept");
-            exit(0);
+
+    printf("Server in ascolto...\n");
+
+    for (;;) {
+        soa = accept(socketfd, (struct sockaddr*)&servizio, &fromlen);
+        if (soa == -1) {
+            perror("accept");
+            continue;
         }
-        
-        read(soa, s, sizeof(s));
-        printf("Stringa ricevuta %s",s);
-        
-        read(soa, s2, sizeof(s2));
-        printf("Stringa ricevuta %s",s2);
-        
+
+        read(soa, s, DIM);
+        read(soa, s2, DIM);
+
         int controllo = controlloStringhe(s, s2);
-        
-        char risposta[DIM];
-        
-        if(controllo == 1){
-            sprintf(risposta, sizeof(risposta), "La stringa 1 è più lunga.");
-        } else if(controllo == 0){
-            sprintf(risposta, sizeof(risposta), "La stringa 2 è più lunga.");
-        } else {
-            sprintf(risposta, sizeof(risposta), "Le due stringhe sono uguali.");
-        }
-        
-        write(socketfd, risposta, sizeof(risposta));
-        
+
+        if (controllo == 1)
+            snprintf(risposta, DIM, "La stringa 1 è più lunga.");
+        else if (controllo == 0)
+            snprintf(risposta, DIM, "La stringa 2 è più lunga.");
+        else
+            snprintf(risposta, DIM, "Le stringhe sono uguali.");
+
+        write(soa, risposta, DIM);
+
         close(soa);
     }
-    
-    return 0;   
+
+    close(socketfd);
+    return 0;
 }
